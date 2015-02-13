6cfb59e659e1960528374aade2e84337a920e393
Martin <marki423@student.liu.se>
2015-02-13 16:55:32 +0100

    Added Euclidean distance calculations

--- lab2/js/kmeans.js
+++ lab2/js/kmeans.js
@@ -7,15 +7,49 @@
    
     function kmeans(data, k) {
 		
+		//1.
+		//Randomly place K points into the space represented by the items that are being clustered. These
+		//points represent the initial cluster centroids.
+		
 		var centroids = new Array(k,3);
 		
         for(i = 0; i<k; i++){
 			
-			var temp = Math.floor(Math.random()*data.length());
+			var temp = Math.floor(Math.random()*data.length);
 			
 			centroids[i] = data[temp];
 		}
         
+		//2.
+		//Assign each item to the cluster that has the closest centroid. There are several ways of calculating
+		//distances and in this lab we will use the Euclidean distance
+		
+		data.forEach(function(d, index){
+			
+			for(i = 0; i<k; i++){
+				var temp =
+				Math.pow(Number(d['A'])
+				- Number(centroids[i]['A']),2)
+				+ Math.pow(Number(d['B'])
+				- Number(centroids[i]['B']),2)
+				+ Math.pow(Number(d['C'])
+				- Number(centroids[i]['C']),2)
+				//+ Math.pow(Number(d['D'])
+				//- Number(centroids[i]['D']),2)
+				//+ Math.pow(Number(d['F'])
+				//- Number(centroids[i]['F']),2)
+				; 
+				
+				console.log("F: " + Number(centroids[i]['A']));
+				
+				temp = Math.sqrt(temp);
+				
+				console.log(temp);	
+			
+			}
+			
+		})
+		
     };
 	
     
