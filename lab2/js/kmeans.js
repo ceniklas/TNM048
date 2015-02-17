8abad7933896e3d5a97a47be537e655688fb118b
Niklas Fransson <zwullo@gmail.com>
2015-02-17 09:38:59 +0100

    did a stuff

--- lab2/js/kmeans.js
+++ lab2/js/kmeans.js
@@ -16,40 +16,58 @@
         for(i = 0; i<k; i++){
 			
 			var temp = Math.floor(Math.random()*data.length);
+			console.log(data[temp]);
 			
 			centroids[i] = data[temp];
 		}
+
+		console.log(centroids);
         
 		//2.
 		//Assign each item to the cluster that has the closest centroid. There are several ways of calculating
 		//distances and in this lab we will use the Euclidean distance
-		
+		var distanceArray = new Array(data.length);
 		data.forEach(function(d, index){
 			
+			var distance = [];
+
 			for(i = 0; i<k; i++){
-				var temp =
+				distance[k] =
 				Math.pow(Number(d['A'])
 				- Number(centroids[i]['A']),2)
 				+ Math.pow(Number(d['B'])
 				- Number(centroids[i]['B']),2)
 				+ Math.pow(Number(d['C'])
-				- Number(centroids[i]['C']),2)
-				//+ Math.pow(Number(d['D'])
-				//- Number(centroids[i]['D']),2)
-				//+ Math.pow(Number(d['F'])
-				//- Number(centroids[i]['F']),2)
-				; 
+				- Number(centroids[i]['C']),2);
 				
-				console.log("F: " + Number(centroids[i]['A']));
+				if(k > 3){
+					distance[k] += Math.pow(Number(d['D'])	- Number(centroids[i]['D']),2) + Math.pow(Number(d['F']) - Number(centroids[i]['F']),2); 
+				}
 				
-				temp = Math.sqrt(temp);
-				
-				console.log(temp);	
-			
+				distance[k] = Math.sqrt(distance[k]);
 			}
+
+			for(j=0; j<distance.length; j++){
+				if(distance[k] < )
+			}
+
+
 			
-		})
-		
+		});
+
+		//3. 
+		//When all objects have been assigned, recalculate the positions of the K centroids to be in the
+		//centre of the cluster. This is achieved by calculating the average values in all dimensions
+		data.forEach(function(d, index){
+
+
+
+
+		});
+
+		console.log("DONE");
+
+
     };
 	
     
