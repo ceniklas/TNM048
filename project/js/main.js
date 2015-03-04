58bfc3b6af501172a03fafb45660c2c92beb74d0
Niklas Fransson <zwullo@gmail.com>
2015-03-04 18:02:05 +0100

    fixed comparing data

--- project/js/main.js
+++ project/js/main.js
@@ -1,16 +1,64 @@
-var dataSet = {};
+var countryData;
+var dataSet = [];
 var map, pie;
 
 var dataLoaded = function() {
+	console.log("Data loaded, calling constructors...");
+	
 	map = new map();
-}
+
+};
 
 loadData(dataSet, dataLoaded);
 
-function loadData(dataSet, callback){
-	var dataLoaded = false;
+function loadData(dataSet, callback){	
+
+
+	var setsToLoad = 2;
+
+	var thingLoaded = function() {
+		setsToLoad--;
+		console.log("----",setsToLoad);
+		if(setsToLoad == 0){
+			console.log("Callbacking!");
+			callback();
+		}
+	}
 
 	//Do stuff and load data.
+	d3.csv("data/Swedish_Population_Statistics.csv", function(data) {
+			
+		for(var i=0; i<data.length; i++){
+			dataSet.push(data[i]);
+		}
+		//dataSet = (data);
 
-	callback();
+		var test1country = (dataSet[9]["region"]).split(" ")[1];
+		console.log(test1country);
+
+		console.log(1);
+        thingLoaded();
+    });
+
+	d3.json("data/swe_mun.json", function(error, sweden) {
+        countryData = sweden["objects"]["swe_mun"]["geometries"];
+
+        console.log(2);
+        thingLoaded();
+        //draw(countries);
+        
+    });
+}
+
+function printFunneyStuff(value){
+	
+
+	for(var i=0; i<dataSet.length; i+=8){
+
+		if(value == (dataSet[i]["region"]).split(" ")[1]){
+			console.log(dataSet[i]);
+		}
+	}
+
+	//console.log(countryData[value].properties.name);
 }
--- project/js/map.js
+++ project/js/map.js
@@ -1,27 +1,20 @@
 function map(){
 
-	var self = this;
+	//var self = this;
 
     var zoom = d3.behavior.zoom().scaleExtent([1, 8]).on("zoom", move);
 
     var mapDiv = $("#map");
 
-    var margin = {top: 20, right: 20, bottom: 20, left: 20},
-        width = mapDiv.width() - margin.right - margin.left,
-        height = mapDiv.height() - margin.top - margin.bottom;
+    var margin = {top: 20, right: 20, bottom: 20, left: 20};
+    var width = mapDiv.width() - margin.right - margin.left;
+    var height = mapDiv.height() - margin.top - margin.bottom;
 
-    //initialize color scale
-    //...
     var colorscale = d3.scale.category20();
-    //initialize tooltip
-    //...
 
     var projection = d3.geo.mercator().center([15, 65]).scale(1000);
 
-    var svg = d3.select("#map").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .call(zoom);
+    var svg = d3.select("#map").append("svg").attr("width", width).attr("height", height).call(zoom);
 
     var path = d3.geo.path().projection(projection);
 
@@ -31,47 +24,12 @@ function map(){
     d3.json("data/swe_mun.json", function(error, sweden) {
         var countries = topojson.feature(sweden, sweden.objects.swe_mun).features;
 
-        //load summary data
-        //...
-
         draw(countries);
-        
     });
-	
-		d3.csv("data/Swedish_Population_Statistics.csv", function(data) {
-			
-			self.data = data;
-
-			
-			
-        // Extract the list of dimensions and create a scale for each.
-        //...
-        //x.domain(dimensions = d3.keys(data[0]).filter(function(d) {
-            //return d!="Country" && (y[d] = d3.scale.linear().domain(d3.extent(data, function(p){return +p[d];})).range([height, 0]));
-        //}));
-
-        drawDataSet();
-		
-		draw();
-    });
-	
-		function drawDataSet(){
-			
-			console.log(self.data);
-		}
 
     function draw(countries,data)
     {
         var country = g.selectAll(".country").data(countries);
-
-        //initialize a color country object	
-        //var cc = {
-            //country: country,
-            //color: colorscale
-        //};
-		
-        //...
-		//var colors = d3.scale.category20();
 
         country.enter().insert("path")
             .attr("class", "country")
@@ -92,19 +50,15 @@ function map(){
             .on("click",  function(d) {
                 selFeature(d.properties.name);
             });
-
     }
     
     //zoom and panning method
     function move() {
-
         var t = d3.event.translate;
         var s = d3.event.scale;
-        
 
         zoom.translate(t);
         g.style("stroke-width", 1 / s).attr("transform", "translate(" + t + ")scale(" + s + ")");
-
     }
     
     //method for selecting features of other components
@@ -113,4 +67,3 @@ function map(){
         lines.push( value );
     }
 }
-
