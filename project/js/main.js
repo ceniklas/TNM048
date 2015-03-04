f2b4986efb83043c443a10556930084f059c1efc
Niklas Fransson <zwullo@gmail.com>
2015-03-04 13:38:12 +0100

    added callback function

--- project/js/main.js
+++ project/js/main.js
@@ -1,16 +1,16 @@
 var dataSet = {};
-
 var map, pie;
 
-
 var dataLoaded = function() {
-  map = new map();
+	map = new map();
 }
 
 loadData(dataSet, dataLoaded);
 
 function loadData(dataSet, callback){
-	
 	var dataLoaded = false;
 
+	//Do stuff and load data.
+
+	callback();
 }
