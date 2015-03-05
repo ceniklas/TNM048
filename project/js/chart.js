3d53ccbad025c57bd59f48325a4650498420e9d0
Niklas Fransson <zwullo@gmail.com>
2015-03-05 17:53:18 +0100

    trieed to fix chart

--- project/js/chart.js
+++ project/js/chart.js
@@ -35,7 +35,7 @@ function drawTheChart(data){
 		.append("svg:g")
 		.attr("transform", "translate(" + r + "," + r + ")");
 		
-	var pie = d3.layout.pie().value(function(d){return d.value;});
+	var pie = d3.layout.pie().value(function(d){ return d.value; });
 
 	// declare an arc generator function
 	var arc = d3.svg.arc().outerRadius(r);
@@ -58,10 +58,60 @@ function drawTheChart(data){
 		});
 
 	// add the text
-	arcs.append("svg:text").attr("transform", function(d){
-				d.innerRadius = 0;
-				d.outerRadius = r;
-		return "translate(" + arc.centroid(d) + ")";}).attr("text-anchor", "middle").text( function(d, i) {
-		return data[i].label;}
-			);
+	arcs.append("svg:text")
+	.attr("transform", function(d){
+		d.innerRadius = 0;
+		d.outerRadius = r;
+		return "translate(" + arc.centroid(d) + ")";})
+	.attr("text-anchor", "middle").text( function(d, i){ return data[i].label; } );
+
+	
+
+
+	/*
+
+	var width = 960;
+    var height = 500;
+    var radius = Math.min(width, height) / 2;
+
+	var color = d3.scale.ordinal().range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+	var arc = d3.svg.arc()
+	    .outerRadius(radius - 10)
+	    .innerRadius(radius - 70);
+
+	var pie = d3.layout.pie()
+	    .sort(null)
+	    .value(function(d) { return d.population; });
+
+	var svg = d3.select("body").append("svg")
+	    .attr("width", width)
+	    .attr("height", height)
+	  .append("g")
+	    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+	d3.csv("data.csv", function(error, data) {
+
+	  data.forEach(function(d) {
+	    d.population = +d.population;
+	  });
+
+	  var g = svg.selectAll(".arc")
+	      .data(pie(data))
+	    .enter().append("g")
+	      .attr("class", "arc");
+
+	  g.append("path")
+	      .attr("d", arc)
+	      .style("fill", function(d) { return color(d.data.age); });
+
+	  g.append("text")
+	      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+	      .attr("dy", ".35em")
+	      .style("text-anchor", "middle")
+	      .text(function(d) { return d.data.age; });
+
+	});
+
+	*/
 }
