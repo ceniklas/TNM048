424790546a5b82f94e0bc811f89a95f1a6ef3916
Makkalakka <antal650@student.liu.se>
2015-03-06 10:32:21 +0100

    en liten push med småändringar i chart

--- project/js/chart.js
+++ project/js/chart.js
@@ -5,6 +5,8 @@ var chartDiv;
 var margin, width, height;
 
 var r;
+
+var data;
 	
 function chart(){
 	/*
@@ -21,41 +23,39 @@ function chart(){
 	
 	r = height/2;
 
-	/*var data = [{"label":"Single", "value":80}, 
+	data = [{"label":"Single", "value":80}, 
 				{"label":"Divorced", "value":15}, 
-				{"label":"Widowed", "value":5}];*/
+				{"label":"Widowed", "value":5}];
 }
 
-function drawTheChart(data){
+function drawTheChart(data2){
 	
-	var vis = d3.select('#chart').append("svg:svg")
+	var vis = d3.select('#chart')
+		.append("svg:svg")
 		.data([data[0]["2000"],data[1]["2000"],data[2]["2000"]])
 		.attr("width", width)
 		.attr("height", height)
 		.append("svg:g")
 		.attr("transform", "translate(" + r + "," + r + ")");
-		
-	var pie = d3.layout.pie().value(function(d){ return d.value; });
+	
 
 	// declare an arc generator function
 	var arc = d3.svg.arc().outerRadius(r);
+	
+	var pie = d3.layout.pie().value(function(d){ return d.value; });
 
-	// select paths, use arc generator to draw
-	var arcs = vis.selectAll("g.slice")  //FUNKAR JUST NU INTE!!!
+	// select paths, use arc generator to draw  //FUNKAR JUST NU INTE!!!
+	var arcs = vis.selectAll("g.slice") 
 		.data(pie)
 		.enter()
 		.append("svg:g")
 		.attr("class", "slice");
-	
+
 	arcs.append("svg:path")
 		.attr("fill", function(d, i){
 			return color(i);
 		})
-		.attr("d", function (d) {
-			// log the result of the arc generator to show how cool it is :)
-			console.log(arc(d));
-			return arc(d);
-		});
+		.attr("d", arc);
 
 	// add the text
 	arcs.append("svg:text")
@@ -64,8 +64,6 @@ function drawTheChart(data){
 		d.outerRadius = r;
 		return "translate(" + arc.centroid(d) + ")";})
 	.attr("text-anchor", "middle").text( function(d, i){ return data[i].label; } );
-
-	
 
 
 	/*
